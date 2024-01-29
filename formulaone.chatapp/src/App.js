ce0f078e63a80cbb43181d7ded6bc5402222f135
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-01-30 00:10:05 +0530

    Frontend : Setup joinChatRoom function and pass it to waiting room component

--- formulaone.chatapp/src/App.js
+++ formulaone.chatapp/src/App.js
@@ -2,9 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';import { Row, Col, Container } from 'react-bootstrap';
 import WaitingRoom from './components/waitingroom';
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
 
 function App() {
+  const[conn,setConnection] = useState();
+  const joinChatRoom = async (username,chatroom) => {
+    try {
+      //initiate a connection
+      const conn = new HubConnectionBuilder()
+                    .withUrl("https://localhost:7135/")
+                    .configureLogging(LogLevel.Information)
+                    .build();
+
+      //set up builder
+      conn.on("JoinSpecificChatRoom",(username,msg) => {
+        console.log("msg: ",msg);
+      })
+
+      await conn.start();
+      await conn.Invoke("JoinSpecificChatRoom",{username,chatroom});
+
+      setConnection(conn);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <div>
       <main>
@@ -16,7 +43,7 @@ function App() {
               </h1>
             </Col>
           </Row>
-          <WaitingRoom></WaitingRoom>
+          <WaitingRoom joinChatRoom={joinChatRoom}></WaitingRoom>
         </Container>
       </main>
     </div>
