51bca14e4304809327aab918ebe69ec338c4e6ff
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2024-01-29 20:57:40 +0530

    Configure signalR Hub to send and recieve joining chat messages

---
+++ FormulaOne.ChatService/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using FormulaOne.ChatService.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace FormulaOne.ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All
+                          .SendAsync("ReciveMessage","admin", $"{conn.Username} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            await Clients.Group(conn.ChatRoom).SendAsync("ReciveMessage","admin",$"{conn.Username} has joined {conn.ChatRoom}");
+        }
+    }
+}
